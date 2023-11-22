a6ac9b02077aab164bd0cc0e81d9e891edd4f070
Lucie Sundermeijer <lucie.sun@gmail.com>
2023-11-22 23:37:57 +0100

    JDA conf

--- src/main/java/com/discordbot/DiscordBotApplication.java
+++ src/main/java/com/discordbot/DiscordBotApplication.java
@@ -1,8 +1,12 @@
 package com.discordbot;
 
 
-public class DiscordBotApplication {
-    public static void main(String[] args) {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
+public class DiscordBotApplication {
+    public static final String TOKEN = System.getenv("TOKEN");
+    public static void main(String[] args) {
+        JDA jda = JDABuilder.createDefault(TOKEN).build();
     }
 }
